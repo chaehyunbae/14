39069ac6d9c1e754f4be1ff8a737ed5c180482c5
chaehyunbae <gotch9359@naver.com>
2020-12-06 23:35:05 +0900

    02

--- main.c
+++ main.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Book {
+	int number;
+	char title[20];
+};
 
 int main(int argc, char *argv[]) {
-	char *pc = NULL;
-	int i = 0;
+	struct Book *p;
+	p = (struct Book*)malloc(2*sizeof(struct Book));
 	
-	pc = (char*)malloc(100*sizeof(char));
-	if (pc == NULL) {
+	if (p==NULL) {
 		printf("memory allocation error!\n");
 		return -1;
 	}
 	
-	for (i=0;i<26;i++)
-	pc[i] = 'a'+i;
+	p->number = 1;
+	strcpy(p->title, "C Programming");
 	
-	pc[i] = 0;
-	printf("%s\n", pc);
-	free(pc);
+	(p+1)->number = 2;
+	strcpy((p+1) ->title, "ELectronics");
+	
+	printf("%s %s\n", p->title, (p+1)->title);
+	
+	free(p);
 	
 	return 0;
 }
